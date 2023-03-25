6bc65f40048d7f79278bad8a4506804de921f9f3
umenatto <shemomechama@yahoo.co.jp>
2023-03-25 22:22:09 +0900

    worksとモーダルのレイアウトを修正したのでコミット

--- main.js
+++ main.js
@@ -63,26 +63,18 @@
 
   // ---------- nav ----------
   const optionsFollow = {
-    // threshold: 0, 対象の要素が見え始める時と見えなくなる時
-    // threshold: .5, 見える範囲が 50% を超えたときのみ検出
-    threshold: [0, 0.1], //見え始めるときと見えなくなる時と、20%を超えた時
+    threshold: [0, 0.1], //見え始めるときと見えなくなる時と、10%を超えた時
     rootMargin: '100px',
   }
 
   const follow = (entry) => {
     const headerHeight = entry[0].boundingClientRect.height;
     const headerTop = -entry[0].boundingClientRect.top;
-    console.log(entry[0]);
-    console.log(entry[0].intersectionRatio);
-    console.log('headerTop:' + headerTop);
     if (headerTop > headerHeight + 100) {
-      console.log('big');
       nav.classList.add('fixed');
     } else if (headerTop > headerHeight + 50) {
-      console.log('middle');
       nav.classList.add('transparent');
     } else {
-      console.log('small');
       nav.classList.remove('fixed');
       nav.classList.remove('transparent');
     }
@@ -90,11 +82,13 @@
   const observerFollow = new IntersectionObserver(follow, optionsFollow);
   observerFollow.observe(header);
 
-  // ---------- モーダル用説明文 ---------- 
-  const instructions = {
-    password: 'パスワード生成アプリです。プログラミング学習サービス「ドットインストール」のレッスンをもとに作りました。<br>レッスンのままのコードだと数字と記号にチェックをいれても確率上入らないことがあるので、その点を改良しました。パスワード生成用の文字列をいったん完成させた後に、1文字(2文字)を数字(記号)に置き換えるという方法で実装しました。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
-    countdown: 'カウントダウンアプリです。プログラミング学習サービス「ドットインストール」のレッスンをもとに作りました。<br>レッスンのままのコードだと決まった秒数でのカウントダウン機能のみだったので、その点を改良しました。秒数を選べて、Start/Stop/Cancelができるようにしました。StartとStopを共通のボタンにするのが難しかったです。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
-    slideshow: 'プログラミング学習サービス「ドットインストール」のレッスンをもとに作ったライドショーです。<br>フォルダ内に写真を追加すると、自動的にスライドショーにも追加されるようにコードをアレンジしました。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
+  // ---------- explanation ---------- 
+  const explanation = {
+    password: 'パスワード生成アプリです。プログラミング学習サービス「ドットインストール」のレッスンをもとに作りました。<br>レッスンのままのコードだと数字と記号にチェックをいれても確率上入らないことがあるので、その点を改良しました。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
+    countdown: 'カウントダウンアプリです。プログラミング学習サービス「ドットインストール」のレッスンをもとに作りました。<br>秒数を選べて、Start/Stop/Cancelができるようにアレンジしました。StartとStopを共通のボタンにするのが難しかったです。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
+    slideshow: 'プログラミング学習サービス「ドットインストール」のレッスンをもとに作ったスライドショーです。<br>フォルダ内に写真を追加すると、自動的にスライドショーに追加されるようにアレンジしました。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
+    portfolio: 'このサイトです。名前があさごはんなので、卵をイメージして、黄色と丸い形をテーマにかわいらしいサイトにしました。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
+    memo: 'プログラミング学習サービス「ドットインストール」のレッスンをもとに作ったメモアプリです。ローカルストレージを利用しています。メモを複数追加できて、個別削除できるようにアレンジしました。<span class="language">使用言語:&nbsp;HTML/CSS/JavaScript</span>',
   }
 
   // ---------- modal ----------
@@ -115,7 +109,7 @@
       img.alt = content.firstElementChild.alt;
       const keyword = content.firstElementChild.dataset.instruction;
       figcaption.classList.add('description');
-      figcaption.innerHTML = instructions[keyword];
+      figcaption.innerHTML = explanation[keyword];
       close.classList.add('bi', 'bi-x-circle');
 
       figure.appendChild(img);
@@ -162,16 +156,5 @@
 
   // ウィンドウをスクロールしたときに handleScroll() 関数を実行
   window.addEventListener("scroll", handleScroll);
-
-  // スクロール量など確認用 ------------------------------------------------------
-  const test1 = document.getElementById('test1');
-  const test2 = document.getElementById('test2');
-  const test3 = document.getElementById('test3');
-  
-  window.addEventListener('scroll', () => {
-    test1.textContent = 'スクロール量:' + window.scrollY;
-    test2.textContent = 'headerのtop位置:' + header.getBoundingClientRect().top;
-    test3.textContent = 'bodyの高さ:' + body.getBoundingClientRect().height;
-  });
 
 }
